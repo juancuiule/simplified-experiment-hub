fbf758682b6b9f492e17a06d30d2c99db91c7e5a
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2023-10-22 03:26:05 -0300

    experiment dashboard

--- src/app/(non-auth)/experiments/[experimentId]/dashboard/page.tsx
+++ src/app/(non-auth)/experiments/[experimentId]/dashboard/page.tsx
@@ -1,3 +1,5 @@
+import { experiments } from "@/app/mock-data";
+import Image from "next/image";
 import Link from "next/link";
 import {
   Activity,
@@ -13,13 +15,34 @@ export default function ExperimentDashboard({
 }: {
   params: { experimentId: string };
 }) {
+  const { name, background } = experiments.find(
+    (e) => e.slug === params.experimentId
+  )!;
   return (
-    <div>
-      <h1>Experiment: {params.experimentId}</h1>
+    <div className="flex flex-col gap-6">
+      <div className="flex items-start justify-start gap-4 flex-col md:flex-row">
+        <div className="aspect-video w-full md:h-full md:w-auto flex justify-center items-center overflow-hidden border border-gray-200 rounded-md">
+          <Image
+            src={background}
+            alt="Team background image"
+            width={200}
+            height={200}
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <h1 className="text-4xl font-bold">{name}</h1>
+          <p className="text-sm">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores
+            ipsam quo mollitia, et praesentium nihil
+          </p>
+        </div>
+      </div>
       <ul>
         <li>
           <Link
             href={`/experiments/${params.experimentId}/analytics`}
+            prefetch
             className="flex gap-2 items-center"
           >
             <Activity size={18} /> <span>Analytics</span>
@@ -28,6 +51,7 @@ export default function ExperimentDashboard({
         <li>
           <Link
             href={`/experiments/${params.experimentId}/edit`}
+            prefetch
             className="flex gap-2 items-center"
           >
             <Edit3 size={18} /> <span>Edit</span>
@@ -36,6 +60,7 @@ export default function ExperimentDashboard({
         <li>
           <Link
             href={`/experiments/${params.experimentId}/flow`}
+            prefetch
             className="flex gap-2 items-center"
           >
             <GitBranch size={18} /> <span>Flow</span>
@@ -44,6 +69,7 @@ export default function ExperimentDashboard({
         <li>
           <Link
             href={`/experiments/${params.experimentId}/views`}
+            prefetch
             className="flex gap-2 items-center"
           >
             <Monitor size={18} /> <span>Views</span>
@@ -52,6 +78,7 @@ export default function ExperimentDashboard({
         <li>
           <Link
             href={`/experiments/${params.experimentId}/data`}
+            prefetch
             className="flex gap-2 items-center"
           >
             <Users size={18} /> <span>Participants and data</span>
@@ -60,6 +87,7 @@ export default function ExperimentDashboard({
         <li>
           <Link
             href={`/experiments/${params.experimentId}/settings`}
+            prefetch
             className="flex gap-2 items-center"
           >
             <Settings size={18} /> <span>Settings</span>
