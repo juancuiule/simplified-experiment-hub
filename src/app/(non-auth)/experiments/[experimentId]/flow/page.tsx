6064b590feaff97f5e008f5fd856c584fe45e75a
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2023-10-23 01:22:35 -0300

    fix styles for view and flow design view

--- src/app/(non-auth)/experiments/[experimentId]/flow/page.tsx
+++ src/app/(non-auth)/experiments/[experimentId]/flow/page.tsx
@@ -6,21 +6,21 @@ import Link from "next/link";
 
 export default function Page({ params }: { params: { experimentId: string } }) {
   return (
-    <div className="flex flex-col flex-1 gap-2.5">
+    <div className="flex flex-col flex-1 gap-2">
       {/* Experiment flow navbar */}
-      <div className="flex justify-between items-center h-12 bg-light rounded p-2.5 gap-2.5">
+      <div className="flex justify-between items-center h-12 bg-light rounded p-2 gap-2">
         <div className="flex justify-center items-center gap-1">
           <Link href={`/experiments/${params.experimentId}/dashboard`}>
             <ChevronLeft size={16} />
           </Link>
           <span className="font-medium">Flow Design</span>
         </div>
-        <div className="flex gap-2.5 text-white">
-          <button className="flex justify-center items-center gap-2 rounded bg-[#4F4F4F] px-2.5 py-1">
+        <div className="flex gap-2 text-white">
+          <button className="flex justify-center items-center gap-2 rounded bg-[#4F4F4F] px-2 py-1">
             <span className="font-medium">Preview</span>
             <Eye size={16} />
           </button>
-          <button className="flex justify-center items-center gap-2 rounded bg-success px-2.5 py-1">
+          <button className="flex justify-center items-center gap-2 rounded bg-success px-2 py-1">
             <span className="font-medium">Save</span>
             <Save size={16} />
           </button>
@@ -28,7 +28,7 @@ export default function Page({ params }: { params: { experimentId: string } }) {
       </div>
 
       {/* Experiment flow design */}
-      <div className="flex flex-1 gap-2.5 max-h-[calc(100vh-64px-24px*2-44px-10px)]">
+      <div className="flex gap-2 max-h-[calc(80vh-64px-24px*2-44px-10px)]">
         {/* Preview */}
         <div
           className="flex-1 bg-light rounded"
@@ -40,7 +40,7 @@ export default function Page({ params }: { params: { experimentId: string } }) {
         ></div>
 
         {/* Nodes menu */}
-        <div className="flex flex-col p-2.5 gap-2.5 overflow-y-scroll rounded bg-light">
+        <div className="flex flex-col p-2 gap-2 overflow-y-scroll rounded bg-light">
           {Object.entries(items).map(([key, value]) => (
             <Fragment key={key}>
               <h4 className="text-md font-semibold">{`${key
--- src/app/(non-auth)/experiments/[experimentId]/views/[viewId]/page.tsx
+++ src/app/(non-auth)/experiments/[experimentId]/views/[viewId]/page.tsx
@@ -26,21 +26,21 @@ export default function Page({
   }, []);
 
   return (
-    <div className="flex flex-col flex-1 gap-2.5">
+    <div className="flex flex-col flex-1 gap-2">
       {/* Experiment flow navbar */}
-      <div className="flex justify-between items-center h-11 bg-light rounded p-2.5 gap-2.5">
+      <div className="flex justify-between items-center h-11 bg-light rounded p-2 gap-2">
         <div className="flex justify-center items-center gap-1">
           <Link href={`/experiments/${params.experimentId}/views`}>
             <ChevronLeft size={16} />
           </Link>
           <span className="font-medium">View Design - {params.viewId}</span>
         </div>
-        <div className="flex gap-2.5 text-white">
-          <button className="flex justify-center items-center gap-2 rounded bg-[#4F4F4F] px-2.5 py-1">
+        <div className="flex gap-2 text-white">
+          <button className="flex justify-center items-center gap-2 rounded bg-[#4F4F4F] px-2 py-1">
             <span className="font-medium">Preview</span>
             <Eye size={16} />
           </button>
-          <button className="flex justify-center items-center gap-2 rounded bg-success px-2.5 py-1">
+          <button className="flex justify-center items-center gap-2 rounded bg-success px-2 py-1">
             <span className="font-medium">Save</span>
             <Save size={16} />
           </button>
@@ -48,7 +48,7 @@ export default function Page({
       </div>
 
       {/* Experiment flow design */}
-      <div className="flex flex-1 gap-2.5 max-h-[calc(100vh-64px-24px*2-44px-10px)]">
+      <div className="flex gap-2 max-h-[calc(80vh-64px-24px*2-44px-10px)]">
         {/* Preview */}
         <div
           className="flex flex-col flex-1 p-6 bg-light rounded h-full overflow-y-scroll"
@@ -75,7 +75,7 @@ export default function Page({
 
         {/* Current widgets */}
         {widgets.length > 0 ? (
-          <div className="flex flex-col p-2.5 gap-2.5 overflow-y-scroll rounded bg-light">
+          <div className="flex flex-col p-2 gap-2 overflow-y-scroll rounded bg-light">
             {widgets.map((widget, i) => {
               const item = fullItems.find(
                 (item) => item.template === widget.template
@@ -120,7 +120,7 @@ export default function Page({
         ) : null}
 
         {/* Nodes menu */}
-        <div className="flex flex-col p-2.5 gap-2.5 overflow-y-scroll rounded bg-light">
+        <div className="flex flex-col p-2 gap-2 overflow-y-scroll rounded bg-light">
           {Object.entries(items).map(([key, value]) => (
             <Fragment key={key}>
               <h4 className="text-md font-semibold">{`${key
