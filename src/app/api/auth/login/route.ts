efda3c62e21fbf42170eeeaa90dcbc27946885aa
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2023-10-29 17:15:30 -0300

    API_URL in auth api routes

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,7 +1,9 @@
+import { API_URL } from "@/constants";
+
 export async function POST(request: Request) {
   const { email, password } = await request.json();
 
-  return await fetch("https://api.experiment-hub.com/users/login", {
+  return await fetch(`${API_URL}/users/login`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -1,26 +1,21 @@
+import { API_URL } from "@/constants";
+
 export async function POST(request: Request) {
   const { email, password, name } = await request.json();
-  console.log({ email, password, name });
 
   try {
-    const data = await fetch("https://api.experiment-hub.com/users", {
+    const data = await fetch(`${API_URL}/users`, {
       method: "POST",
       body: JSON.stringify({
-        email: "juani@gmail.com",
-        password: "abc123",
+        email,
+        password,
+        name,
         type: "SCIENTIST",
-        name: "Juani",
       }),
       headers: {
         "Content-Type": "application/json",
-      }
-    })
-      .then((res) => res.json())
-      .catch((err) => {
-        console.log(err);
-      });
-
-    console.log("Algo");
+      },
+    }).then((res) => res.json());
 
     return data;
   } catch (error) {
