3c784e7f2e5b7aec92540eb5dc46600459431c07
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2023-11-27 03:37:50 -0300

    fix cors error on options

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,11 +8,21 @@ export async function middleware(request: NextRequest) {
     }`
   );
 
-  const response = NextResponse.next();
-
-  if (request.nextUrl.pathname.startsWith("/api")) {
-    response.headers.append("Access-Control-Allow-Origin", "*");
+  if (request.method === "OPTIONS") {
+    const response = new NextResponse(null, {
+      status: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers":
+          "Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version",
+        "Access-Control-Max-Age": "86400",
+      },
+    });
+    return response;
   }
+
+  const response = await NextResponse.next();
 
   return response;
 }
