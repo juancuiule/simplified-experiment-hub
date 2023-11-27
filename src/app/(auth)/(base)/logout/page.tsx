2a594924f631b4f4f6ef4dffc8bf1eab62df1f8c
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2023-11-27 10:55:32 -0300

    fix error

--- src/app/(auth)/(base)/logout/page.tsx
+++ src/app/(auth)/(base)/logout/page.tsx
@@ -1,6 +1,8 @@
 import { API } from "@/api";
 import { redirect } from "next/navigation";
 
+export const dynamic = 'force-dynamic';
+
 export default async function Page() {
   await API.auth.logout({});
   redirect("/login");
