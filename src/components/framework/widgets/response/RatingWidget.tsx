3502df7a74fff81d015af5418b1c69a5ca554b46
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2023-11-26 22:21:28 -0300

    update rating widget

--- src/components/framework/widgets/response/RatingWidget.tsx
+++ src/components/framework/widgets/response/RatingWidget.tsx
@@ -3,52 +3,60 @@ import { RatingWidget } from "@/lib/widgets/response";
 import { useFormikContext } from "formik";
 
 export default function Rating(props: { widget: RatingWidget }) {
-  const { errors, touched, submitCount, handleChange } =
-    useFormikContext<
-      Record<string, string | boolean | number | string[] | undefined>
-    >();
+  const { errors, touched, submitCount, handleChange, handleBlur, values } =
+    useFormikContext<Record<string, string | undefined>>();
 
-  const { label, max, optionsLabel, dataKey } = props.widget.props;
+  const { label, max, optionsLabel = [], dataKey } = props.widget.props;
 
   const options = Array.from({ length: max }, (_, i) => {
     return {
       value: `${i + 1}`,
       label: i + 1,
+      description: optionsLabel.find((o) => parseInt(o.value) === i + 1)?.label,
     };
   });
 
   return (
-    <div className="flex flex-col gap-2">
-      {label !== "" ? (
-        <div className="text-md">
-          <Markdown
-            allowedElements={["strong", "em", "del", "p", "span"]}
-            content={label}
-          />
-        </div>
-      ) : null}
-      <div className="flex flex-row justify-between w-full gap-2">
-        {options.map((option) => {
-          return (
-            <label
-              key={option.value}
-              htmlFor={`${dataKey}-${option.value}`}
-              className="flex justify-center items-center"
-            >
-              <input
-                type="radio"
-                name={dataKey}
-                id={`${dataKey}-${option.value}`}
-                value={option.value}
-                onChange={handleChange}
-                className="peer opacity-0 w-0 h-0"
+    <div className="flex flex-col gap-6">
+      <div className="flex flex-col gap-2 w-full">
+        <div className="flex flex-col gap-1">
+          {label !== "" ? (
+            <label className="text-md font-medium" htmlFor={dataKey}>
+              <Markdown
+                allowedElements={["strong", "em", "del", "p", "span"]}
+                content={label}
               />
-              <div className="cursor-pointer peer-checked:border-primary peer-checked:text-primary peer-checked:font-bold border-gray-400 text-gray-400 w-9 flex justify-center items-center aspect-square rounded-full border-[3px]">
-                <span>{option.label}</span>
-              </div>
             </label>
-          );
-        })}
+          ) : null}
+          <div className="flex flex-row justify-between w-full gap-2 items-start">
+            {options.map((option) => {
+              return (
+                <label
+                  key={option.value}
+                  htmlFor={`${dataKey}-${option.value}`}
+                  className="flex justify-center items-center flex-col flex-1"
+                >
+                  <input
+                    type="radio"
+                    name={dataKey}
+                    id={`${dataKey}-${option.value}`}
+                    value={option.value}
+                    onChange={handleChange}
+                    className="peer opacity-0 w-0 h-0"
+                  />
+                  <div className="cursor-pointer peer-checked:border-primary peer-checked:text-primary peer-checked:font-bold border-gray-200 text-gray-400 w-9 flex justify-center items-center aspect-square rounded-full border-[3px]">
+                    <span>{option.label}</span>
+                  </div>
+                  {option.description && (
+                    <span className="text-xs text-center max-w-full leading-4">
+                      {option.description}
+                    </span>
+                  )}
+                </label>
+              );
+            })}
+          </div>
+        </div>
       </div>
       {errors[dataKey] && (touched[dataKey] || submitCount > 0) ? (
         <div className={"text-error text-xs"}>{errors[dataKey] || ""}</div>
