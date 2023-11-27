4b1be3762b789ca09a301d92b3371bcd39e9de03
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2023-11-27 01:45:03 -0300

    fix data table with all columns

--- src/app/(non-auth)/experiments/[experimentId]/data/FlatTable.tsx
+++ src/app/(non-auth)/experiments/[experimentId]/data/FlatTable.tsx
@@ -2,10 +2,23 @@
 import { Answer } from "@/api";
 import { Grid } from "@githubocto/flat-ui";
 
+const parseAnswers = (answers: Answer[]) => {
+  const columns = Array.from(
+    new Set(answers.flatMap((answer) => Object.keys(answer.body)))
+  );
+  const rows = answers.map((answer) => ({
+    id: answer.id,
+    ...(Object.fromEntries(
+      columns.map((column) => [column, answer.body[column]])
+    ) as Record<string, string>),
+  }));
+  return rows;
+};
+
 export default function FlatTable(props: { data: Answer[] }) {
   return (
-    <div className="h-full flex flex-col flex-1">
-      <Grid data={props.data.map((d) => ({ id: d.id, ...d.body }))}></Grid>
+    <div className="h-full flex flex-col flex-1 font-mono">
+      <Grid data={parseAnswers(props.data)}></Grid>
     </div>
   );
 }
